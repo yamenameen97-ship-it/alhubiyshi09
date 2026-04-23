fd23a33a7ea471a307efdfa80d23f60702ce6f15
yamenameen97-ship-it <yamenameen97@gmail.com>
2026-04-24 02:01:38 +0300

    Add files via upload

--- static/js/server.js
+++ static/js/server.js
@@ -1,3 +1,16 @@
+
+const fs = require('fs');
+const path = require('path');
+const dataPath = path.join(__dirname, '../../data.json');
+
+function readData() {
+    return JSON.parse(fs.readFileSync(dataPath, 'utf8'));
+}
+
+function writeData(data) {
+    fs.writeFileSync(dataPath, JSON.stringify(data, null, 2));
+}
+
 const express=require("express");
 const cors=require("cors");
 const bcrypt=require("bcrypt");
